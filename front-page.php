027678593bbfdcbf31c428979b96617b7673a3cd
Sean Butze <sbutze@gmail.com>
2012-06-11 14:32:54 -0400

    Add default slides

--- front-page.php
+++ front-page.php
@@ -4,26 +4,50 @@ Template Name: Home Page
 */
 ?>
 <?php get_header(); ?>
-			<div id="feature" class="row">
-				<div id="slideshow">
-					<?php $slides = new WP_Query(array( 'post_type' => 'slide')); ?>
-               		<?php while ($slides->have_posts() ) : $slides->the_post(); ?>
 
-						<div class="slide clearfix">
-							<div class="slide-text five columns">
-								<div class="container float-left">
-									<h1><?php the_title();?></h1>
-									<?php the_content();?>
-								</div>
-							</div>
-						</div><!--/ .slide -->
+<div id="feature" class="row">
+	<div id="slideshow">
+		<?php $slides = new WP_Query(array( 'post_type' => 'slide')); ?>
 
-					<?php endwhile; wp_reset_query(); ?>
+		<?php if ($slides->have_posts() ): ?>
 
-				</div><!--/ #slideshow -->
-			</div>
+	   		<?php while ($slides->have_posts() ) : $slides->the_post(); ?>
+
+				<div class="slide clearfix">
+					<div class="slide-text five columns">
+						<div class="container float-left">
+							<h1><?php the_title();?></h1>
+							<?php the_content();?>
+						</div>
+					</div>
+				</div><!--/ .slide -->
+
+			<?php endwhile; wp_reset_query(); ?>
+		<?php else: ?>
+
+				<div class="slide clearfix">
+					<div class="slide-text five columns">
+						<div class="container float-left">
+							<h1>Lorem ipsum dolor sit amet</h1>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+						</div>
+					</div>
+				</div><!--/ .slide -->
+				<div class="slide clearfix">
+					<div class="slide-text five columns">
+						<div class="container float-left">
+							<h1>Dolore magna aliquam erat volutpat.</h1>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+						</div>
+					</div>
+				</div><!--/ .slide -->
+
+		<?php endif; ?>
+
+	</div><!--/ #slideshow -->
+</div><!-- / #feature -->
 
 
-				<?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 				
 <?php get_footer(); ?>
