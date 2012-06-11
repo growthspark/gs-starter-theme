5b82dd2f4111c43b71f5b52c686f375ef964ff3f
Sean Butze <sbutze@gmail.com>
2012-06-11 14:32:16 -0400

    Created 404 template

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<?php get_template_part('content', 'page-top'); ?>
+
+<article>
+	<h1>404 Not Found</h1>
+	<p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
+	<?php get_search_form(); ?>
+</article>
+
+<?php get_template_part('content', 'page-bottom'); ?>
+<?php get_footer(); ?>
