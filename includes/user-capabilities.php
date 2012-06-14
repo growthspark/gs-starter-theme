82afbff76fbc688ba42527550549b5d9ec82eb2a
Sean Butze <sbutze@gmail.com>
2012-06-14 16:04:35 -0400

    Removed "Hide Admin Tabs", moved into admin-menu.php

--- includes/user-capabilities.php
+++ includes/user-capabilities.php
@@ -2,7 +2,10 @@
 /*********************************************************************
 **                              
 ** Custom User Capabilities                                    
-**                                     
+** 
+** For full list of capabilities, see:
+** http://codex.wordpress.org/Roles_and_Capabilities  
+**                                
 **********************************************************************/
 
 global $wp_roles;
@@ -13,6 +16,7 @@ if (class_exists('WP_Roles'))
 
 if (is_object($wp_roles)) {
 
+
 	/* --------------------------------------------------------------------
 	:: Administrator
 	-------------------------------------------------------------------- */
@@ -22,32 +26,12 @@ if (is_object($wp_roles)) {
 	/* --------------------------------------------------------------------
 	:: Editor
 	-------------------------------------------------------------------- */
-	$wp_roles->add_cap( 'editor', 'edit_theme_options' );
 	$wp_roles->remove_cap( 'editor', 'manage_links' );
+	$wp_roles->add_cap( 'editor', 'edit_theme_options' );
+	
 
 }
 
-/* --------------------------------------------------------------------
-
-:: Hide Admin Tabs
-
-Can be used as a work-around to hide tabs from the admin menu
-when modifying capabilities isn't an option.
-
--------------------------------------------------------------------- */
-function gs_remove_admin_menus(){
-
-    if ( function_exists('remove_menu_page') ) { 
-
-    	// Remove the Tools tab for all users below Admin
-    	if ( !current_user_can('remove_users') ) {
-        remove_menu_page('tools.php');  
-    	}
-
-    }
-
-}
-add_action('admin_menu', 'gs_remove_admin_menus'); 
 
 
 
