f4128e669181100d28072ff6038c6848f0bfaed1
Sean Butze <sbutze@gmail.com>
2012-06-17 11:06:43 -0400

    Default sidebar content removed

--- includes/sidebars-config.php
+++ includes/sidebars-config.php
@@ -5,10 +5,8 @@
 
 I. Set Widget Templates
 II. Build Widget Areas
-III. Set Default Sidebar Content
 
 ***************************************************************/
-
 
 /* ----------------------------------------------------------
 
@@ -68,69 +66,5 @@ if ( function_exists('register_sidebars') )
         'before_title' => gs_before_title(),
         'after_title' => gs_after_title(),
      ));
-
-
-/* ----------------------------------------------------------
-
-III. Set Default Sidebar Content
-
-Used in sidebar.php to generate default widgets when none have 
-been set by the user
-
-------------------------------------------------------------- */
-function gs_default_home_widgets() {
-
-  for ($i = 1; $i <= 3; $i++) {
-
-      the_widget('GS_Feature_Box',
-        array(
-              'title' => 'Lorem Ipsum',
-              'text' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
-              'linktext' => 'Read More',
-              'linkurl' => '#',
-              'filter' => 1,
-            ),
-        array( 
-          'before_widget' => gs_home_before_widget(),
-              'after_widget' => gs_home_after_widget(),
-              'before_title' => gs_before_title(),
-              'after_title' => gs_after_title()
-            )
-      );
-
-  }
-
-}
-
-function gs_default_sidebar_widgets() {
-
-    the_widget('WP_Widget_Search',
-      array(
-        
-          ),
-      array( 
-        'before_widget' => gs_before_widget(),
-            'after_widget' => gs_after_widget(),
-            'before_title' => gs_before_title(),
-            'after_title' => gs_after_title()
-          )
-    );
-
-      the_widget('GS_Feature_Box',
-        array(
-              'title' => 'Lorem Ipsum',
-              'text' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
-              'linktext' => 'Read More',
-              'linkurl' => '#',
-              'filter' => 1,
-            ),
-        array( 
-          'before_widget' => gs_before_widget(),
-              'after_widget' => gs_after_widget(),
-              'before_title' => gs_before_title(),
-              'after_title' => gs_after_title()
-            )
-      );
-}
 
 ?>
