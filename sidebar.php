cfc04399bc6716095679bb360c60f0ac599e559d
Sean Butze <sbutze@gmail.com>
2012-06-22 14:09:01 -0400

    Elminated default widgets

--- sidebar.php
+++ sidebar.php
@@ -3,32 +3,20 @@
 <?php if( is_front_page() ):?>
 
 	<aside id="home-widgets" class="row">
-		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Homepage Widgets') ): 
-
-				gs_default_home_widgets();
-
-		endif; ?>
+		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Homepage Widgets') ); ?>
 
 	</aside>
 	
 <?php elseif ( is_singular('post') ):?>
 
 	<aside id="sidebar" class="four columns">
-		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Blog Sidebar') ) :
-
-				gs_default_sidebar_widgets();
-
-		endif; ?>
+		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Blog Sidebar') ); ?>
 	</aside>	
 
 <?php else:?>
 
 	<aside id="sidebar" class="four columns">
-		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Page Sidebar') ) :
-
-				gs_default_sidebar_widgets();
-
-		endif; ?>	
+		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Page Sidebar') ); ?>	
 	</aside>
 
 <?php endif;?>
