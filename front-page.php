fce5ec67e401117e8a04b28f695d99077bb6479b
Sean Butze <sbutze@gmail.com>
2012-06-22 14:05:59 -0400

    Removed default slides

--- front-page.php
+++ front-page.php
@@ -5,13 +5,12 @@ Template Name: Home Page
 ?>
 <?php get_header(); ?>
 
-<div id="feature" class="row">
+<div id="feature" class="row feature-row">
 	<div id="slideshow">
 		<?php $slides = new WP_Query(array( 'post_type' => 'slide')); ?>
 
-		<?php if ($slides->have_posts() ): ?>
 
-	   		<?php while ($slides->have_posts() ) : $slides->the_post(); ?>
+	   		<?php if ($slides->have_posts() ): while ($slides->have_posts() ) : $slides->the_post(); ?>
 
 				<div class="slide clearfix">
 					<div class="slide-text five columns">
@@ -22,27 +21,8 @@ Template Name: Home Page
 					</div>
 				</div><!--/ .slide -->
 
-			<?php endwhile; wp_reset_query(); ?>
-		<?php else: ?>
+			<?php endwhile; endif; wp_reset_query(); ?>
 
-				<div class="slide clearfix">
-					<div class="slide-text five columns">
-						<div class="container float-left">
-							<h1>Lorem ipsum dolor sit amet</h1>
-							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-						</div>
-					</div>
-				</div><!--/ .slide -->
-				<div class="slide clearfix">
-					<div class="slide-text five columns">
-						<div class="container float-left">
-							<h1>Dolore magna aliquam erat volutpat.</h1>
-							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-						</div>
-					</div>
-				</div><!--/ .slide -->
-
-		<?php endif; ?>
 
 	</div><!--/ #slideshow -->
 </div><!-- / #feature -->
