5e052c50446d51ea1733e9bdaedc01cb20754b9c
Sean Butze <sbutze@gmail.com>
2012-06-27 15:06:09 -0400

    Removed Modernizr tests, moved into functions.php

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,6 @@ Global Site Scripts
 
 /* ----------------------------------------------------------
 
-:: Modernizr Tests
-
-We can use Modernizr.load to load certain scripts &
-libraries only when they're needed.
-
--------------------------------------------------------------*/
-
-/* Support CSS Selectors in IE 6-8 */
-Modernizr.load([{
-
-		// Test for border-radius support (effectively tests for IE 6-8)
-	    test : Modernizr.borderradius,
-	    // Load Selectivizr to enable CSS selectors in IE 6-8
-	    nope : [templateurl + '/js/selectivizr.min.js']
-
-}]);
-
-
-/* ----------------------------------------------------------
-
 :: jQuery Scripts
 
 Set up jQuery scripts needed on all site pages.  
