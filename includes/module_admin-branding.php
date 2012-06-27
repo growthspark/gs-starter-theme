d89ac7bd59e0b5130924ebece36b1bc7143b1485
Sean Butze <sbutze@gmail.com>
2012-06-27 16:32:54 -0400

    Added function_exists checkers for custom headers

--- includes/module_admin-branding.php
+++ includes/module_admin-branding.php
@@ -18,20 +18,22 @@ Replaces the WordPress logo on the login screen with a logo of
 your choosing.
 
 -------------------------------------------------------------------- */
-function my_custom_login_logo() {
-    ?><style type="text/css">
-        h1 a { 
-        	background-image:url('<?php header_image(); ?>') !important; 
-        	background-size: <?php echo get_custom_header()->width; ?>px <?php echo get_custom_header()->height; ?>px !important;
-        	height: <?php echo get_custom_header()->height; ?>px !important;
-        	width: <?php echo get_custom_header()->width; ?>px !important;
-        	padding: 0 !important;
-        	margin: 0 auto 20px !important;
-    	}
-    </style>
-    <?php
+if (function_exists('get_custom_header')) {
+    add_action('login_head', 'my_custom_login_logo');
+    function my_custom_login_logo() {
+        ?><style type="text/css">
+            h1 a { 
+            	background-image:url('<?php header_image(); ?>') !important; 
+            	background-size: <?php echo get_custom_header()->width; ?>px <?php echo get_custom_header()->height; ?>px !important;
+            	height: <?php echo get_custom_header()->height; ?>px !important;
+            	width: <?php echo get_custom_header()->width; ?>px !important;
+            	padding: 0 !important;
+            	margin: 0 auto 20px !important;
+        	}
+        </style>
+        <?php
+    }
 }
-add_action('login_head', 'my_custom_login_logo');
 
 /* --------------------------------------------------------------------
 
@@ -55,26 +57,29 @@ Adds a logo to the top of the Dashboard page, replacing the standard
 
 -------------------------------------------------------------------- */
 
-add_action('admin_head', 'gs_add_logo_to_dashboard');
-function gs_add_logo_to_dashboard() {
-    ?>
-    <style>
-    .index-php .wrap h2:nth-child(2) {
-        visibility: hidden;
-        line-height: 1px;
-    }
+if (function_exists('get_custom_header')) {
+    add_action('admin_head', 'gs_add_logo_to_dashboard');
+    function gs_add_logo_to_dashboard() {
+        ?>
+        <style>
+        .index-php .wrap h2:nth-child(2) {
+            visibility: hidden;
+            line-height: 1px;
+        }
 
-    .index-php #icon-index {
-        background-image:url('<?php header_image(); ?>') !important; 
-        background-position: 0px 0px !important;
-        height: <?php echo get_custom_header()->height; ?>px !important;
-        width: <?php echo get_custom_header()->width; ?>px !important;
-        float: none !important;
-        margin-top: 15px !important;
+        .index-php #icon-index {
+            background-image:url('<?php header_image(); ?>') !important; 
+            background-position: 0px 0px !important;
+            height: <?php echo get_custom_header()->height; ?>px !important;
+            width: <?php echo get_custom_header()->width; ?>px !important;
+            float: none !important;
+            margin-top: 15px !important;
+        }
+        </style>
+        <?php
     }
-    </style>
-    <?php
 }
+
 
 
 ?>
