ad7deb222b04c0e83883a643671c197d4cea0a20
Sean Butze <sbutze@gmail.com>
2012-07-18 11:33:06 -0400

    Removed default recent_drafts as this is already removed now in recent-drafts-widget.php

--- includes/admin/remove-dashboard-widgets.php
+++ includes/admin/remove-dashboard-widgets.php
@@ -14,9 +14,9 @@ function gs_remove_dashboard_widgets() {
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+	
 
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
-	//unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 
