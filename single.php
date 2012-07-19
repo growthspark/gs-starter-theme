ba7675b1dbb64c8e187ec6b35387508fd02cda15
Sean Butze <sbutze@gmail.com>
2012-07-19 16:02:04 -0400

    Added a single.php default template

---
+++ single.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<?php get_template_part('templates/subpage-top'); ?>
+
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part('templates/post'); ?>
+
+				<?php endwhile; endif; ?>
+
+				<?php get_template_part('templates/comments-template'); ?>
+
+<?php get_template_part('templates/subpage-bottom'); ?>
+
+<?php get_footer(); ?>
