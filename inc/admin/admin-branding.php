0d9ac54803198aaad0c651906022486f51bfdfbc
Sean Butze <sbutze@gmail.com>
2012-11-21 11:31:20 -0500

    Converted to class

--- inc/admin/admin-branding.php
+++ inc/admin/admin-branding.php
@@ -1,79 +1,85 @@
 <?php
+
 /**
  * Admin Branding
  *
- * This file contains functions for branding various areas of
- * the WordPress admin panels with custom logos.
- */
-
-/**
- * Adds a custom logo to the login screen
+ * This class adds a custom logo to the dashboard & login screen of WordPress.
  *
- * @uses growthspark_get_default_logo_options()
- * @uses get_option()
  */
-function gs_custom_login_logo() {
-    $defaults = growthspark_get_default_logo_options();
-    $option = get_option('growthspark_logo_options', $defaults);
+class GS_Admin_Branding {
 
-    ?>
-    <!-- Custom Login Logo -->
-    <style type="text/css">
-        .login h1 a { 
-        	background-image:url("<?php echo gs_get_logo(); ?>") !important; 
-        	background-size: <?php echo gs_logo_width(); ?>px <?php echo gs_logo_height(); ?>px !important;
-        	height: <?php echo gs_logo_height(); ?>px !important;
-        	width: <?php echo gs_logo_width(); ?>px !important;
-        	padding: 0 !important;
-        	margin: 0 auto 20px !important;
-    	}
-    </style>
-    <!--/ Custom Login Logo -->
-    <?php
-}
-add_action('login_head', 'gs_custom_login_logo');
+    private $logo_path;
+    private $logo_width;
+    private $logo_height;
 
+    function __construct() {
 
-/**
- * Change login logo URL
- *
- * Changes the link on the login logo to the site's own URL, instead 
- * of WordPress.org.
- */
-function gs_custom_login_url() {
-  return site_url();
-}
-add_filter( 'login_headerurl', 'gs_custom_login_url', 10, 4 );
+        $this->logo_path = get_bloginfo('template_url') . '/img/logo.png';
+        $this->logo_width = 216;
+        $this->logo_height = 80;
 
-
-/**
- * Adds a custom logo to the Dashboard
- *
- * @uses growthspark_get_default_logo_options()
- * @uses get_option()
- */
-function gs_add_logo_to_dashboard() {
-    $defaults = growthspark_get_default_logo_options();
-    $option = get_option('growthspark_logo_options', $defaults);
-    ?>
-    <!-- Custom Dashboard Logo -->
-    <style type="text/css">
-    .index-php .wrap h2:nth-child(2) {
-        visibility: hidden;
-        line-height: 1px;
+        add_action('login_head', array($this, 'login_logo'));
+        add_filter('login_headerurl', array($this, 'login_url'), 10, 4);
+        add_action('admin_head', array($this, 'dashboard_logo'));
     }
 
-    .index-php #icon-index {
-        background-image:url("<?php echo gs_get_logo(); ?>") !important; 
-        background-size: <?php echo gs_logo_width(); ?>px <?php echo gs_logo_height(); ?>px !important;
-        background-position: 0px 0px !important;
-        height: <?php echo gs_logo_height(); ?>px !important;
-        width: <?php echo gs_logo_width(); ?>px !important;
-        float: none !important;
-        margin-top: 15px !important;
+    /**
+     * Adds a custom logo to the login screen
+     */
+    function login_logo() {
+        ?>
+        <!-- Custom Login Logo -->
+        <style type="text/css">
+            .login h1 a { 
+                background-image:url("<?php echo $this->logo_path; ?>") !important; 
+                background-size: <?php echo $this->logo_width; ?>px <?php echo $this->logo_height ?>px !important;
+                height: <?php echo $this->logo_height ?>px !important;
+                width: <?php echo $this->logo_width; ?>px !important;
+                padding: 0 !important;
+                margin: 0 auto 20px !important;
+            }
+        </style>
+        <!--/ Custom Login Logo -->
+        <?php
     }
-    </style>
-    <!--/ Custom Dashboard Logo -->
-    <?php
+
+    /**
+     * Change login logo URL
+     *
+     * Changes the link on the login logo to the site's own URL, instead 
+     * of WordPress.org.
+     */
+    function login_url() {
+      return site_url();
+    }
+
+    /**
+     * Adds a custom logo to the Dashboard
+     *
+     */
+    function dashboard_logo() {
+        ?>
+        <!-- Custom Dashboard Logo -->
+        <style type="text/css">
+        .index-php .wrap h2:nth-child(2) {
+            visibility: hidden;
+            line-height: 1px;
+        }
+
+        .index-php #icon-index {
+            background-image:url("<?php echo $this->logo_path; ?>") !important; 
+            background-size: <?php echo $this->logo_width; ?>px <?php echo $this->logo_height ?>px !important;
+            background-position: 0px 0px !important;
+            height: <?php echo $this->logo_height ?>px !important;
+            width: <?php echo $this->logo_width; ?>px !important;
+            float: none !important;
+            margin-top: 15px !important;
+        }
+        </style>
+        <!--/ Custom Dashboard Logo -->
+        <?php
+    }
+
 }
-add_action('admin_head', 'gs_add_logo_to_dashboard');
+
+new GS_Admin_Branding;
